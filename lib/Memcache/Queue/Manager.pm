c2699e635afe10304594c468fca7af07e75944ee
星野 将 <masartz@gmail.com>
2009-03-10 00:46:45 +0900

    change key make

--- lib/Memcache/Queue/Manager.pm
+++ lib/Memcache/Queue/Manager.pm
@@ -12,33 +12,70 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 no MooseX::WithCache;
 
+use constant {
+    QUEUE_EXPIRE => (60 * 60 ),
+    CLASS_CNT_EXPIRE => (60 * 60 * 24),
+};
+
 sub enqueue{
     my ($self, $work_class , $arg) = @_;
 
-    $self->cache_set( $work_class , $arg , 100 );
+    my $cnt = $self->_assign_cnt( $work_class );
 
-    return 1;
+    my $key = $self->_make_key( $work_class , $cnt );
+
+    $self->cache_set( $key , $arg , QUEUE_EXPIRE );
+
+    return $cnt;
 
 }
 
-sub dequeue{
-    my ($self, $work_class , $arg) = @_;
+sub _get_cnt{
+    my ($self, $work_class ) = @_;
+
+    return $self->cache_get( $work_class );
 }
 
-sub work_once{
+sub _assign_cnt{
+    my ($self, $work_class ) = @_;
+
+    my $max_cnt = $self->_get_cnt( $work_class );
+
+    my $key_cnt = $max_cnt ? ++$max_cnt : 1;
+
+    $self->cache_set( $work_class ,  $key_cnt , CLASS_CNT_EXPIRE );
+
+    return $key_cnt;
+}
+
+sub _make_key{
+    my ($self, $work_class , $cnt ) = @_;
+
+    return sprintf("%s_%s" , $work_class , $cnt );
+}
+
+
+sub work_start{
     my ($self, $work_class) = @_;
 
-    my $arg = $self->cache_get( $work_class );
+    my $cnt = $self->_get_cnt( $work_class );
+    
+    while(1){
+        my $key = $self->_make_key($work_class, $cnt);
+        my $arg = $self->cache_get( $key );
 
-    return if ! $arg;
+        last if ! $arg;
 
-    my $work_ret = $work_class->work_safely( $self , $arg );
+        $self->cache_del( $key );
 
-    if( $work_ret ){
-        $self->cache_del( $work_class );
+        $work_class->work_safely( $self , $arg );
+
+        $cnt--;
     }
 
     return 1;
 
 }
+
+
 1;
--- t/01_basic.t
+++ t/01_basic.t
@@ -1,23 +1,27 @@
 use strict;
-use Test::More tests => 7;
+use warnings;
+
+use Test::More tests => 3;
 use Test::Output qw/ stdout_is /;
 use Memcache::Queue;
+
+# memcache clear
+{
+    use Cache::Memcached;
+    my $mem = Cache::Memcached->new({
+        servers => 'localhost:11211'
+    });
+    $mem->flush_all();
+}
 
 my $mem_q = Memcache::Queue->new();
 isa_ok( $mem_q , 'Memcache::Queue' , 'object new OK');
 can_ok( $mem_q , qw/ manager / );
 
 my $manager = $mem_q->manager;
-isa_ok( $manager , 'Memcache::Queue::Manager' , 'Manager object OK');
-can_ok( $manager , qw/ enqueue dequeue work_once / );
-
-my $cache = $manager->cache;
-isa_ok( $cache , 'Cache::Memcached' , 'Cache object OK');
-can_ok( $manager , qw/ cache_get cache_set cache_del 
-                     cache_get_multi cache_incr cache_decr / );
 
 $manager->enqueue('Worker::Test', {'arg'=>'TTTEEESSSTTT'} );
-stdout_is( sub { $manager->work_once( 'Worker::Test' );  } , 'TTTEEESSSTTT');
+stdout_is( sub { $manager->work_start( 'Worker::Test' );  } , 'TTTEEESSSTTT');
 
 
 package Worker::Test;
@@ -27,7 +31,6 @@ sub work {
     my ($class, $job) = @_;
 
     print STDOUT $job->{arg};
-#    $job->completed;
 
     return 1;
 }
